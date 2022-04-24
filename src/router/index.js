de022ea9bd9938e9f50ce4441bba57a37d25236d
Freddy Alberto Morales <fredalbert37@outlook.com>
2022-04-24 00:41:55 -0500

    changing the directory for some views

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,42 @@
-import PageHome from '@/components/PageHome.vue'
-import PageThreadShow from '@/components/PageThreadShow.vue'
 import {createRouter, createWebHistory} from 'vue-router'
+import Home from '@/components/pages/Home.vue'
+import ThreadShow from '@/components/pages/ThreadShow.vue'
+import NotFound from '@/components/pages/NotFound.vue'
+import SourceData from '@/data.json'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: PageHome
+    component: Home
   },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    component: PageThreadShow,
-    props: true
+    component: ThreadShow,
+    props: true,
+    beforeEnter (to, _from, next) {
+      //check iof thread exists
+      const threadExists = SourceData.threads.find(threads => threads.id === to.params.id)
+      //if exists continue
+      if(threadExists) {
+        return next()
+      }else{
+        //if doesnt exist redirect to 404
+        return next({
+          name: 'NotFound',
+          params: { pathMatch: to.path.substring(1).split('/') },
+          query: to.query,
+          hash: to.hash
+        })
+      }
+    }
   },
+  {
+    path:'/:patchMatch(.*)*',
+    name:'NotFound',
+    component: NotFound
+  }
 
 ]
 
